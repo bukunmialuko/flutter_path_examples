6bf13f4600050d57de1f65f1877b9d05a707ff82
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2022-01-13 13:44:53 +0100

    some solution

--- README.md
+++ README.md
@@ -4,13 +4,6 @@ A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+https://stackoverflow.com/questions/55704331/what-is-the-difference-between-custompainter-and-customclipper#_=_
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+https://www.developerlibs.com/2019/08/flutter-draw-custom-shaps-clip-path.html
--- lib/src/example_1/root.dart
+++ lib/src/example_1/root.dart
@@ -6,16 +6,55 @@ class Example1RootWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: 200,
-            height: 100,
-            color: Colors.blue,
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+                child: Stack(
+              children: [
+                // Container(
+                //   width: 300,
+                //   height: 100,
+                //   color: Colors.yellow,
+                // ),
+                ClipPath(
+                  clipper: MyCustomClipper(),
+                  child: Container(
+                    width: 300,
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    const circleWidth = 72.0;
+    Path path = Path();
+
+    path.moveTo(0, 18);
+    path.lineTo(0, (size.height));
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, (18));
+    path.lineTo(0, 18);
+    path.addOval(Rect.fromCircle(
+        center: Offset(size.width / 2, (size.height / 2) - 14),
+        radius: circleWidth / 2));
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
